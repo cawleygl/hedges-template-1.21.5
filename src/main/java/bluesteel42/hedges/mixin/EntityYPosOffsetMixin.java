1950b4f4d659f29c0d066710785f8cd441774dcb
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-12-10 19:35:32 -0500

    Mixin changes - prevented mocking vanilla functionality

--- src/main/java/bluesteel42/hedges/mixin/EntityYPosOffsetMixin.java
+++ src/main/java/bluesteel42/hedges/mixin/EntityYPosOffsetMixin.java
@@ -20,22 +20,13 @@ public class EntityYPosOffsetMixin {
 		PosAccessor posAccessor = (PosAccessor) (Object) this;
 		if (thisObject.supportingBlockPos.isPresent()) {
 			BlockPos blockPos = (BlockPos)thisObject.supportingBlockPos.get();
-			if (!(offset > 1.0E-5F)) {
-				cir.setReturnValue(blockPos);
-				cir.cancel();
-			} else {
+			if ((offset > 1.0E-5F)) {
 				BlockState blockState = thisObject.getEntityWorld().getBlockState(blockPos);
-				cir.setReturnValue((!(offset <= 0.5) || !blockState.isIn(BlockTags.FENCES)) && !blockState.isIn(BlockTags.WALLS) && !blockState.isIn(ModTags.Blocks.HEDGES) && !(blockState.getBlock() instanceof FenceGateBlock)
-						? blockPos.withY(MathHelper.floor(posAccessor.getPos().y - offset))
-						: blockPos);
-				cir.cancel();
+				if (!blockState.isIn(ModTags.Blocks.HEDGES)) {
+					cir.setReturnValue(blockPos);
+					cir.cancel();
+				}
 			}
-		} else {
-			int i = MathHelper.floor(posAccessor.getPos().x);
-			int j = MathHelper.floor(posAccessor.getPos().y - offset);
-			int k = MathHelper.floor(posAccessor.getPos().z);
-			cir.setReturnValue(new BlockPos(i, j, k));
-			cir.cancel();
 		}
 	}
 }
--- src/main/java/bluesteel42/hedges/mixin/FenceGateOffsetMixin.java
+++ src/main/java/bluesteel42/hedges/mixin/FenceGateOffsetMixin.java
@@ -3,7 +3,6 @@ package bluesteel42.hedges.mixin;
 import bluesteel42.hedges.util.ModTags;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FenceGateBlock;
-import net.minecraft.registry.tag.BlockTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class FenceGateOffsetMixin {
 	@Inject(at = @At("HEAD"), method = "isWall", cancellable = true)
 	private void init(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-			cir.setReturnValue(state.isIn(BlockTags.WALLS) || state.isIn(ModTags.Blocks.HEDGES));
+		if (state.isIn(ModTags.Blocks.HEDGES)) {
+			cir.setReturnValue(true);
 			cir.cancel();
+		}
 	}
 }
